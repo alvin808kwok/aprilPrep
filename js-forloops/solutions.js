4c8f307929c1e14242ca424f412e738470724d43
alvin808kwok <alvin808kwok@gmail.com>
2019-11-12 11:13:52 -1000

    DevLeague Prep

---
+++ js-forloops/solutions.js
@@ -0,0 +1,348 @@
+/* 1) Ticket Generator
+Write a for-loop that will iterate through 20 numbers (starting at 1 and ending at 20) and console.log the following message:
+"Now serving 1."
+"Now serving 2."
+"Now serving 3."
+.
+.
+.
+"Now serving 20."
+*/
+
+for(var i = 1; i < 21; i++){
+  console.log("Now serving " + i);
+}
+
+
+/* 2) Pop Charts
+Write a for-loop that will iterate through the topFive array below and console.log the following message:
+This week's chart buster is: 'Closer.'
+This week's chart buster is: 'Starboy.'
+This week's chart buster is: 'I Feel It Coming.'
+This week's chart buster is: 'Let Me Love You.'
+This week's chart buster is: '24K Magic.'
+*/
+
+var topFive = ["Closer", "Starboy", "I Feel It Coming", "Let Me Love You", "24K Magic"];
+
+for(var i = 0; i<topFive.length; i++){
+  console.log("This week's chart buster is: " + "'"+ topFive[i] +".'");
+}
+
+/* 3) Dead Presidents
+Declare a variable named `presidents` and assign it to an array containing the following Presidents: Washington, Adams, Jefferson, Madison and Monroe.
+ 
+Write a for-loop that will iterate through this array and within the for-loop console.log the following:
+The value at 0 is Washington.
+The value at 1 is Adams.
+The value at 2 is "Jefferson.
+The value at 3 is "Madison.
+The value at 4 is Monroe.
+
+
+Next, console.log the length of the array.
+
+3-A) Create a function named leaders which will take in a parameter: `person`.
+   @param Datatype: String `person`
+ 
+This function will iterate through the person parameter and console.log the following message for each item in the array:
+
+"President person was a great leader."
+*/
+
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for(var i = 0; i<presidents.length; i++){
+  console.log("The value at " + i + " is " + presidents[i]);
+}
+
+console.log(presidents.length);
+
+function leaders(person){
+  for(var i = 0; i<person.length; i++){
+    console.log("President " + person[i] + " was a great leader.");
+  }
+}
+
+leaders(presidents);
+
+
+
+/* 4) Line Number
+Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
+
+Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+
+var stringOfNumbers = "";
+for(var i = 10; i<21; i++){
+  stringOfNumbers += i;
+}
+
+console.log(stringOfNumbers);
+
+
+/* 5) Even Stevens
+Declare a variable named `evenNumArr` and assign its value to an empty array. 
+
+Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
+Console.log your results.
+*/
+
+var evenNumArr = [];
+
+for(var i = 0; i<100; i++){
+  if(i%2 === 0){
+    evenNumArr.push(i);
+  }
+}
+
+console.log(evenNumArr);
+
+/* 6) Up the Odds
+Declare a variable named `oddSum` and assign it to the Number value 0.
+
+Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
+Console.log your results.
+*/
+  
+ var oddSum = 0;
+ for(var i = 1; i<100; i++){
+  if(i%2 === 1){
+    oddSum +=i;
+  }
+ }
+
+ console.log(oddSum);
+
+
+/* 7) Oops There It is
+Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
+Note that every odd index value in `oopsArray` is currently `undefined`. Using a for-loop, add the string `'nope'` to every odd index. Console.log your result. It should look like this:
+
+[ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
+*/
+
+var oopsArray = [ 'turn' , , 'down' , , 'for' , , 'what' ];
+for(var i = 0; i<oopsArray.length; i++){
+  console.log(oopsArray[i]);
+  if(i%2 ===1){
+    oopsArray[i] = "nope";
+  }
+}
+
+console.log(oopsArray);
+
+
+/* 8) Is It There Oops
+Using a for-loop, iterate through the Array stored at `oopsArray` backwards. Console.log your result. It should look like this:
+
+what
+nope
+for
+nope
+down
+nope
+turn
+*/
+
+for(var i = oopsArray.length - 1; i>=0; i--){
+  console.log(oopsArray[i]);
+}
+
+
+/* 9) Siesta Time
+Declare a variable named `napSchedule` and assign its value to the following array: `[false, false, true, false, true, true]`
+
+Next, write a function named `nap`. This function takes in a single parameter: `schedule`
+
+   @param Datatype: Array `schedule`
+
+Inside of this function write a for-loop that will iterate through the `napSchedule` array and console.log the message: `ZzZzZzZz` if the schedule is `true`, otherwise the it will console.log the message: `Gotta get coding!` if the schedule is `false`.
+*/
+
+var napSchedule = [false, false, true, false, true, true];
+
+function nap(schedule){
+  for(var i = 0; i<schedule.length; i++){
+   if(schedule[i] === true){
+    console.log("ZzZzZzZz");
+   }else{
+    console.log("Gotta get coding!");
+   } 
+  }
+}
+
+nap(napSchedule);
+
+
+/* 10) Copy Pasta
+Declare a variable named `valuesArray` and assign its value to be an array: `[99, 66, 829, 1941, 8, 76]`. 
+Declare another variable named `copyValuesArray` and assign its value to an empty array.
+
+Write a function named `copyArray` which takes two arguments: `originArray` and `destinationArray`. 
+
+   @param Datatype: Array `originArray`
+   @param Datatype: Array `destinationArray`
+
+Inside of this function write a for-loop that will iterate through the contents of the `originArray` and pushes each element of that array into `destinationArray`. Console.log your result.
+*/
+
+var valuesArray = [99, 66, 829, 1941, 8, 76];
+var copyValuesArray = [];
+
+function copyArray(originArray, destinationArray){
+  for(var i = 0; i<originArray.length; i++){
+    destinationArray.push(originArray[i]);
+  }
+  return destinationArray;
+}
+
+console.log(copyArray(valuesArray, copyValuesArray));
+console.log(copyValuesArray);
+
+
+/*Final Boss*/
+
+/* 11) Go Long
+Declare a variable named `topQuote` and assign it to a String value of your favorite one line quote.
+Write a function that will iterate through the string value and return the longest word in that quote. Console.log your result.
+*/
+
+var topQuote = "Boards don't hit back.";
+
+function longestWord(str){
+  var strToArray = str.split(" "); //converts string to an array
+  var currentWord = ""; //to store the string 
+  for(var i =0; i<strToArray.length; i++){ //iterate through the string array
+    if(strToArray[i].length > currentWord.length){ //comparing if the word's length is longer than current word
+      currentWord = strToArray[i]; 
+    }
+  }
+  return currentWord;
+}
+
+console.log(longestWord(topQuote));
+
+/* 12) Puppet Master
+Declare a variable named `miscStorage` set it's value to be: `[ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ]`
+Write a function named `generateArrayOfStrings` which takes a single argument `storage`. This function will return a new Array with only `String` values inside of it.
+
+   @param Datatype: Array `storage`
+   @return Datatype: Array
+
+*/
+
+var miscStorage = [ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ];
+
+function generateArrayOfStrings(storage){
+  var stringArray = [];
+  for(var i = 0; i<storage.length; i++){
+    if(typeof storage[i] === typeof ""){ //typeof operator returns a string https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+      stringArray.push(storage[i]);
+    }
+  }
+  return stringArray;
+}
+
+console.log(generateArrayOfStrings(miscStorage));
+
+
+/* 13) All Grown Up 
+Write a function that will capitalize the first letter in each word in the phrase below. The function will console.log the message: "I've Lived A Life That's Full. I've Traveled Each and Every Highway. But More, Much More Than This. I Did It My Way."  
+*/
+
+var myWay = "i've lived a life that's full, i've traveled each and every highway. but more, much more than this. i did it my way.";
+
+function capitalize(str){
+  var word = str.split(" ");
+  //console.log(word);
+  for(var i = 0; i<word.length; i++){
+    //console.log(word[i]);
+    var letter = word[i].split("");
+    //console.log(letter);
+    letter[0] = letter[0].toUpperCase();
+    //console.log(letter);
+    word[i] = letter.join('');
+  }
+  return word.join(" ");
+}
+
+console.log(capitalize(myWay));
+
+/* 14) Sightseeing
+Write a function that will loop through the multi-dimensional array and console.log the city and landmark on separate lines.
+Note, please use two for loops to complete this problem
+
+  @param Datatype: Array
+  */
+
+var guide = [["Honolulu", "Waikiki"], ["Tokyo", "Tsukiji Fish Market"], ["Cairo", "Pyramids"],["Rome", "Colosseum"]];
+
+for(var i = 0; i<guide.length; i++){
+  for(var j = 0; j<guide[i].length; j++){
+    console.log(guide[i][j])
+  }
+}
+
+
+/* 15) Back to School
+Declare a variable named `currentCohort` and set it's value to be this [array found here](https://gist.github.com/sgnl/e40879b2249e06ca7811).
+Write a function named `graduateAndSetNewClass`, which takes a single argument called `cohort`.
+
+   @param Datatype: Array `cohort`
+
+The function will iterate through the `cohort` argument and check each student's `enrolled` property.
+If the `enrolled` property is set to `true` then change that student's `graduated` property to `true`. Otherwise, if `enrolled` is set to `false` then change `enrolled` to `true` leaving `graduated` alone and unchanged.
+Console.log your result.
+*/
+
+var currentCohort = [
+  {
+    name: 'Doug',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Pat',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Marsha',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Moira',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Ben',
+    graduated: false,
+    enrolled: true
+  },
+  {
+    name: 'Nigel the Giraffe',
+    graduated: false,
+    enrolled: false
+  },
+  {
+    name: 'Brandon the Shark',
+    graduated: false,
+    enrolled: true
+  }
+];
+
+function graduateAndSetNewClass(cohort){
+ for(var i = 0; i<cohort.length; i++){
+  //console.log(cohort[i]);
+  if(cohort[i].enrolled === true){
+    cohort[i].graduated = true;
+  }else{
+    cohort[i].graduated = false;
+  }
+ }
+ return cohort;
+}
+
+console.log(graduateAndSetNewClass(currentCohort));
---
+++ js-objects/solutions.js
@@ -0,0 +1,408 @@
+/*
+Objects in Javascript can be compared to objects in real life. Objects has a collection of properties associated with it (known as key-value pairs) and are basically the same as ordinary variables.
+*/ 
+
+
+//A literal object.
+
+/*
+1.  Declare a variable named `books` and create the following properties (key-value pairs to the object:
+    - add a key named `title` and a string value of "The Obstacle is the Way"
+    - add a key named `author` and a string value of "Ryan Holiday"
+    - add a key named `category` and string value of "Self Help"
+    - add a key named `pages` and number value of 200
+    
+    Console.log your object.
+*/
+
+var books = {
+  title: "The Obstacle is the Way",
+  author: "Ryan Holiday",
+  category: "Self Help",
+  pages: 200
+};
+
+console.log(books);
+    
+  
+/*
+2.  Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
+   
+    - add a key named `name` and set it to a string value of your choice
+    - add a key named `age` and set it to a number value of your choice
+    - add a key named `vegeterian` and set it to a boolean value of your choice
+    - add a key named `color` and set it to an array with two string elements in it
+    - add a key named `speak` which will be a method that return a string "bark!"; 
+        
+ Console.log the following phrase by accessing the appropriate object properties:
+ 
+ 
+ "My dog `name` is `age` year old and likes to `speak` at strangers."
+*/ 
+
+var dog = {
+  name: "Snoopy",
+  age: 253,
+  vegeterian: false,
+  color: ["white", "black"],
+  speak: function(){
+    return "bark!";
+  }
+};
+
+console.log("My dog " + dog.name + " is " + dog.age + " year old and likes to " + dog.speak() + " at strangers.");
+
+
+//An empty object
+
+/*
+3. Declare a variable named `kicks` and assign it to an EMPTY object.
+   Next you will add the following properties to this object. Each property declaration expression will be on its own line and
+   ending with semicolons (`;`). 
+            - add a property named `brand` and set it to your favorite sneaker brand
+            - add a property named `color` and set it the value "cornflower blue"
+            - add a property named `size` and set it to your shoe size
+            - add a property named `price` and set it to a number value between 50 to 100
+            - add a property named `buy` and set it to a method that will return a string "Now I is a cool kid!"
+            
+  Console.log the object.
+*/
+
+var kicks = {};
+kicks.brand = "vivo footwear";
+kicks.color = "cornflower blue";
+kicks.size = 11;
+kicks.price = 88;
+kicks.buy = function(){
+  return "Now I is a cool kid!";
+}
+
+console.log(kicks);
+
+/* 
+
+4. Declare a variable named `plainBox` and set its value to be an EMPTY object.
+   Next, you will add three properties to this object. 
+            - add a property named `color` and set it the name of any color.
+            - add a property named `size` and set it to a number value between `0` and `20`.
+            - add a property named `contents` and set it to be an empty array.
+
+            console.log the object
+*/
+
+var plainBox = {};
+plainBox.color = "red";
+plainBox.size = 18;
+plainBox.contents = [];
+
+console.log(plainBox);
+
+
+/*
+5. Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
+            - add a key named `model` and set its value to the name of an American car manufacturer.
+            - add a key named `year` and set it to be a value above 2000 but below 2016.
+            - add a key named `automaticTransmission` and give it a Boolean value of your choice.
+            - add a key named `driver` and set it to be `null`.
+            - add a key named `passengers` and set it to be an empty array.
+*/
+
+var stockCar = {
+  model: "Tesla",
+  year: 2016,
+  automaticTransmission: true,
+  driver: null,
+  passengers: []
+};
+
+console.log(stockCar);
+
+/*
+
+6. Add new property inside a function
+   Declare a variable named `plainPerson`. Set its value to be an EMPTY object with no properties.
+
+   Next, declare a function named `buildPerson` and then define three parameters. The first parameter will be named `person` 
+   and the second will be `nameString` and the third will be `age`. Within this function you will modify the `person` object 
+   by:
+            - adding a property called `name` and set its value to be the second parameter.
+            - adding a property called `age` and set its value to be the third parameter.
+
+   The function will return the first parameter.
+   Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store 
+   the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
+   the value at `name`, and just the value at `age`.
+ */
+
+ var plainPerson = {};
+
+ function buildPerson(person, nameString, age){
+    person.name = nameString;
+    person.age = age;
+
+    return person;
+
+ }
+
+ var completePerson = buildPerson(plainPerson, "Charlie Brown", 80);
+ console.log(completePerson);
+
+
+/*
+7. Display values of objects that are inside an array
+   Declare a new variable named `arrayOfObjects` and set it to be [this array of objects
+   (https://gist.github.com/sgnl/958adf99007329d2e4ff).
+
+   Declare a new function named `printOrders`and set one parameter called `orders`. Within this function you 
+   will return a string that prints the order details to the page in the following format.
+
+        Example output:
+            =====
+            id:  0
+            purchase date:  Monday Jan 25 2015 2:01 PM
+            purchase total:  279.38
+            =====
+            id:  1
+            purchase date:  Monday Jan 27 2015 11:31 AM
+            purchase total:  79.80
+            =====
+            id:  2
+            purchase date:  Monday Feb 1 2015 7:56 AM
+            purchase total:  15.62
+            =====
+            ...
+ */
+
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+function printOrders(orders){
+  for(var i = 0; i<orders.length; i++){
+    console.log("=====");
+    console.log("id: " + orders[i].id);
+    console.log("date: " + orders[i].date);
+    console.log("total: " + orders[i].total);
+  }
+
+}
+
+printOrders(arrayOfObjects);
+
+/*
+8. Addition with an object
+   Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. 
+   Properties 'a' and 'b' will be set to a number and result will be 'undefined.'
+
+   Declare a new function named objectAddition. Declare the first parameter, which will be an object. Within this 
+   function, you will access the values stored `a` and `b` of the object being passed in. Add the these values and store 
+   the sum to this object's `result` property. This function will return the object.
+
+        Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log` 
+        to inspect your results.
+*/
+
+var sumObj = {
+  a: 3,
+  b: 5,
+  result: undefined
+};
+
+function objectAddition(obj){
+  obj.result = obj.a + obj.b;
+  return obj;
+
+}
+
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
+
+/*
+9. Print sum function and add as new key-value
+   Declare a new function named printObj and a single parameter which will be the object from the challenge just above. 
+   Within this function you are to print to the screen a message of the operation performed. For Example:
+
+        // if this object was passed into your function:
+        { a: 10, b:67, result: 77 }
+        // it should print a message saying
+        '10 + 67 = 77'
+
+        Before returning this object, add a new property to it named `output` and set it to be the message that was printed 
+        out.
+
+        Invoke this function and pass in your object. Further test by changing the values of the object being passed in or 
+        **create more** objects and invoke your function multiple times.
+ */
+
+function printObj(obj){
+  return obj.a + " + " + obj.b + " = " + obj.result;
+}
+
+console.log(printObj(sumObj));
+/*
+10. Putting stuff in `plainBox`
+        Declare a function named putInPlainBox and a single parameter which will be an object. Within this function, write a 
+        FOR loop that adds **10** random number values to the array referenced at the `contents` property of the object being 
+        passed in. This function will return the object.
+
+        Invoke your function and pass in your object (which should be `plainBox`), store the result to a variable named 
+        plainBoxResult and use `console.log` to inspect your results.
+ */
+
+function putInPlainBox(obj){
+   for(var i =0; i<10; i++){
+      var randomNum = Math.floor(Math.random()*10);
+      obj.contents.push(randomNum);
+   } 
+   return obj;
+}
+
+var plainBoxResult = putInPlainBox(plainBox);
+console.log(plainBoxResult);
+
+
+/*
+11. Detecting transmission
+    Declare a function named detectingTransmission and a single parameter which will be an object. Within this function 
+    you will check to see if the car has an automatic or manual transmission and print the results on screen.
+
+    If `automaticTransmission` is true then print a message saying so. Also, provide an appropriate message for when the 
+    its false.
+
+    Invoke your function and pass in your stockCar object, store the result to a variable named isAutomaticTransmission and use `console.log` to inspect your results.
+ */
+
+function detectingTransmission(obj){
+  if(obj.automaticTransmission === true){
+    return "Relax kid, it's automatic";
+  }else{
+    return "Manual process son";
+  }
+}
+
+var isAutomaticTransmission = detectingTransmission(stockCar);
+console.log(isAutomaticTransmission);
+
+/*
+12.  Who's driving this thing?!
+     As you may have noticed that the `stockCar` doesn't have a driver!
+
+     Declare a function named addDriver with two parameters. The first parameter will be an object with represents a 
+     **car**, the other will be a **person**. Within this function, set the `driver` value of the **stockCar** to the 
+     second parameter being passed into your function.
+
+     Invoke your function and pass in your objects, store the result to a variable named stockCarWithDriver, and inspect 
+      your results. Consider using `plainPerson` as your driver.
+ */
+
+function addDriver(obj, person){
+  obj.driver = person;
+  return obj;
+}
+
+var stockCarWithDriver = addDriver(stockCar, plainPerson);
+console.log(stockCarWithDriver);
+
+/*
+    #Final Boss
+    The Dev League instructors want to ride your whip!
+
+        Declare a variable named `passengerList` and set it to be `['Jon', 'Jason', 'Tony', 'Joe', 'Jesse', 'Nigel', 'Kelli', 'Marifel', 'Victor']`
+
+        Declare a variable named `passengerAges` and set it to be `[19, 12, 21, 22, 16, 9, 19, 20, 15]`
+
+        Declare a function named `addPassengers` with three parameters. The first will be a **car** and the second will be an array of **names** and the third will be an array of **ages**. The names and ages are in sequence, e.g. "Jon" is "19", "Jason" is "12".
+
+        In the end you will return the **car** but within the function...
+
+        You will have to populate the `passengers` array on the **car** object with proper objects that represent a person. Currently you have two arrays, one which contains names and another which contains ages.
+
+        You should iterate through the names and ages, pass the values to your `buildPerson` function to build person objects (remember that this function returns a new object). Don't forget that this function actually takes **three** arguments, how will you handle that? (you should not have to change your function).
+
+    Example of a loaded Car:
+
+    # Display passengers
+        Delcare a function named `displayPassengers` and set one parameter which will be a **car**. This function should print out each passenger's name and age one line at a time.
+
+    example output:
+        'Jon, age 19, is riding dirty!'
+        'Jason, age 12, is riding dirty!'
+        'Tony, age 21, is riding dirty!'
+        'Joe, age 22, is riding dirty!'
+        'Jesse, age 16, is riding dirty!'
+        'Nigel, age 9, is riding dirty!'
+        'Kelli, age 19, is riding dirty!'
+        'Marifel, age 19, is riding dirty!'
+        'Victor, age 19, is riding dirty!'
+ */
+
+var passengerList = ['Jon', 'Jason', 'Tony', 'Joe', 'Jesse', 'Nigel', 'Kelli', 'Marifel', 'Victor'];
+
+var passengerAges = [19, 12, 21, 22, 16, 9, 19, 20, 15];
+
+function addPassengers(car, names, ages){
+  for(var i = 0; i<names.length; i++){
+    var newRider = {};
+    buildPerson(newRider, names[i], ages[i]);
+    car.passengers.push(newRider);
+  }
+  return car;
+}
+
+addPassengers(stockCar, passengerList, passengerAges);
+console.log(stockCar);
+
+
+
+
+
+
